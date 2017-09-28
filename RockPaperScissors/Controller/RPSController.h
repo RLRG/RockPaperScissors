50c105e8ef368f7cbc75df698bf6bc752529394d
Rodrigo López-Romero Guijarro <rlromero@gmv.com>
2017-09-28 12:45:07 +0200

    feat: add the game controller entity

---
+++ RockPaperScissors/Controller/RPSController.h
@@ -0,0 +1,13 @@
+//
+//  RPSController.h
+//  RockPaperScissors
+//
+//  Created by Rodrigo López-Romero Guijarro on 28/09/2017.
+//  Copyright © 2017 Rodrigo López-Romero Guijarro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPSController : NSObject
+
+@end
---
+++ RockPaperScissors/Controller/RPSController.m
@@ -0,0 +1,18 @@
+//
+//  RPSController.m
+//  RockPaperScissors
+//
+//  Created by Rodrigo López-Romero Guijarro on 28/09/2017.
+//  Copyright © 2017 Rodrigo López-Romero Guijarro. All rights reserved.
+//
+
+#import "RPSController.h"
+#import "RPSTurn.h"
+
+@implementation RPSController
+
+-(void)throwDown:(Move) move {
+    // TODO: Body of the function.
+}
+
+@end
