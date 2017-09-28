d8d19cc18a602fd3337bbaf4f5deb4694fb86552
Rodrigo López-Romero Guijarro <rlromero@gmv.com>
2017-09-28 12:37:33 +0200

    Initial Commit

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Rodrigo López-Romero Guijarro on 28/09/2017.
+//  Copyright © 2017 Rodrigo López-Romero Guijarro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
