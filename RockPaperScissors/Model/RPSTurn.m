59d846e49f258fe71af8d90aba8a9ab006f409ca
Rodrigo López-Romero Guijarro <rlromero@gmv.com>
2017-09-28 13:06:36 +0200

    feat: generate random move
    Moreover, a new case in the enumeration is added for invalid cases.

--- RockPaperScissors/Model/RPSTurn.m
+++ RockPaperScissors/Model/RPSTurn.m
@@ -24,8 +24,22 @@
 }
 
 -(Move)generateMove {
-    // TODO: Change the following placeholder:
-    return Rock;
+    NSUInteger randomNumber = arc4random_uniform(3);
+    
+    switch (randomNumber) {
+        case 0:
+            return Rock;
+            break;
+        case 1:
+            return Paper;
+            break;
+        case 2:
+            return Scissors;
+            break;
+        default:
+            return Invalid;
+            break;
+    }
 }
 
 -(BOOL)defeats:(RPSTurn*) turn {
